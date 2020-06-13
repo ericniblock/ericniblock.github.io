ddda2f382f0ab8e82e9a1b529bf6d439a3bd6f38
ericniblock <ericnibl@buffalo.edu>
2020-06-13 17:21:48 -0400

    Version 1.0.2

--- java_script.js
+++ java_script.js
@@ -1,19 +1,29 @@
-var letters = []
+var letters = [];
 
 for (i = 0; i < 11; i++) {
-    letters.push(Math.random());
-  }
+  letters.push(Math.random());
+}
 
-$(document).ready(function(){
-    var i;
-    for (i = 0; i < letters.length; i++) {
-        $('.slide-in-blurred-top-'+String(i+1)).css({'-webkit-animation' : 'slide-in-blurred-top ' + (letters[i]+0.5) + 's cubic-bezier(0,.26,.95,.6) both;'})
-        $('.slide-in-blurred-top-'+String(i+1)).css({'animation' : 'slide-in-blurred-top ' + (letters[i]+0.5)+ 's cubic-bezier(0,.26,.95,.6) both'})
-        console.log('.slide-in-blurred-top-'+String(i+1))
-        console.log(letters[i]+0.5)
-    }
+$(document).ready(function () {
+  var i;
+  for (i = 0; i < letters.length; i++) {
+    $(".slide-in-blurred-top-" + String(i + 1)).css({
+      "-webkit-animation":
+        "slide-in-blurred-top " +
+        letters[i] +
+        "s cubic-bezier(0,.26,.95,.6) both;",
+    });
+    $(".slide-in-blurred-top-" + String(i + 1)).css({
+      animation:
+        "slide-in-blurred-top " +
+        letters[i] +
+        "s cubic-bezier(0,.26,.95,.6) both",
+    });
+    console.log(".slide-in-blurred-top-" + String(i + 1));
+    console.log(letters[i] + 0.5);
+  }
 });
 
-setTimeout(function() {
-    $('.flex-c').addClass('fade-out');
-}, 2000);
+setTimeout(function () {
+  $(".intro-container").addClass("fade-out");
+}, 1000);
